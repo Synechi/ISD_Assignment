169a823c30f5e15c49d0604b3827dc1a40ece0dd
harris184 <43329615+harris184@users.noreply.github.com>
2019-05-22 18:52:55 +1000

    Update DB.java

--- Source Packages/Model/dao/DB.java
+++ Source Packages/Model/dao/DB.java
@@ -8,8 +8,8 @@ import java.sql.Connection;
  */
 public abstract class DB {
     protected String URL = "jdbc:derby://localhost:1527/";//replace empty-string with jdbc:derby local host url
-    protected String db = "OMSDB";//name of the database
-    protected String dbuser = "isduser";//db root user
+    protected String db = "omsdb1";//name of the database
+    protected String dbuser = "omsuser";//db root user
     protected String dbpass = "admin"; //db root password
     protected String driver = "org.apache.derby.jdbc.ClientDriver"; //jdbc client driver - built in with NetBeans
     protected Connection conn; //connection null-instance to be initialized in sub-classes
