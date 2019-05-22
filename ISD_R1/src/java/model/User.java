a1fd9f7f4f76e9b96b047cf7bab8cb6fb97a0487
Synechi <45441635+Synechi@users.noreply.github.com>
2019-05-22 23:57:10 +1000

    Fresh Netbeans Project

---
+++ ISD_R1/src/java/model/User.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+/**
+ *
+ * @author Ben
+ */
+public class User {
+    private String ID;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String postcode;
+    private String dob;
+    private int accType;
+
+    public User(String ID, String name, String email, String username, String password, String address, String city, String state, String country, String postcode, String dob, int accType) {
+        this.ID = ID;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.postcode = postcode;
+        this.dob = dob;
+        this.accType = accType;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getAccType() {
+        return accType;
+    }
+
+    public void setAccType(int accType) {
+        this.accType = accType;
+    }
+    
+}
